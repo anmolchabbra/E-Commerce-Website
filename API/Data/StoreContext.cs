2380c0647e6302cf88ba94c17ae441c9e0794273
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2023-10-30 11:22:11 -0400

    Created Product Controller and added API to get Products and other API to get specific product with the productID.

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,9 +1,18 @@
 
 
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Data
 {
     public class StoreContext : DbContext
     {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+ 
+        public DbSet<Product> Products {get; set;}
         
+
     }
 }
